0b31fdba7f0c074cddff777f975b17b70abf7bf2
Lukasz Bekus <beckon@interia.pl>
2020-04-25 15:23:54 +0200

    Dodaj pliki projektów.

---
+++ Dziennik_v0.1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dziennik_v0._1.MvcApplication" Language="C#" %>
---
+++ Dziennik_v0.1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dziennik_v0._1.Startup))]
+namespace Dziennik_v0._1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

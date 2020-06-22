a33a5e252df41f7999a74f1444600717553e6e6d
kukulim <beckon@interia.pl>
2020-06-22 21:13:57 +0200

    roczny wykres działą

--- Dziennik_v0.1/Controllers/StatisticsController.cs
+++ Dziennik_v0.1/Controllers/StatisticsController.cs
@@ -1,4 +1,5 @@
 ﻿using Dziennik_v0._1.Core;
+using Dziennik_v0._1.Core.ViewModels.StatisticsViewModels;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -18,9 +19,41 @@ namespace Dziennik_v0._1.Controllers
         }
         public ActionResult Index()
         {
+            return View();
+        }
+        public JsonResult TraningsLengthList()
+        {
             var userId = User.Identity.GetUserId();
+
+            var viewModel = new TraningsLengthListViewModel();
+
+            var WorkoutList = _unitOfWork.Workouts.GetAllWorkouts(userId).ToList();
+
+            for (int i = 0; i < 12; i++)
+            {
+                var buffor = WorkoutList.Where(c => c.Date.Month == (i + 1)).ToList();
+                var buffor2 = 0;
+                foreach (var item in buffor)
+                {
+                    buffor2 += (Convert.ToInt32(item.LengthOfTraining));
+                }
+                viewModel.Workouts.Add(buffor2);
+            };
+
             var CardioList = _unitOfWork.Cardios.GetAllCardios(userId).ToList();
-            return View(CardioList);
+
+            for (int i = 0; i < 12; i++)
+            {
+                var buffor = CardioList.Where(c => c.Date.Month == (i + 1)).ToList();
+                var buffor2 = 0;
+                foreach (var item in buffor)
+                {
+                    buffor2+=(Convert.ToInt32(item.LengthOfTraining));
+                }
+                viewModel.Cardios.Add(buffor2);
+            };
+
+            return Json(viewModel, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ Dziennik_v0.1/Core/ViewModels/StatisticsViewModels/TraningsLengthListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Dziennik_v0._1.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dziennik_v0._1.Core.ViewModels.StatisticsViewModels
+{
+    public class TraningsLengthListViewModel
+    {
+        public List<int> Cardios { get; set; } = new List<int>();
+        public List<int> Workouts { get; set; } = new List<int>();
+    }
+}

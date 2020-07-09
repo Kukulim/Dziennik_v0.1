988308827a9b67da6abb1a7eef97ad1cd4cde3a7
kukulim <beckon@interia.pl>
2020-07-09 19:45:15 +0200

    dodany controler dieta

---
+++ Dziennik_v0.1/Controllers/DietController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dziennik_v0._1.Controllers
+{
+    public class DietController : Controller
+    {
+        // GET: Diet
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

fb658c55735329cebcf5ff239b3566825d51b3c6
kukulim <beckon@interia.pl>
2020-07-15 19:20:05 +0200

    AchievementsApiController + Leyout zmiana by pokazywało nowe achievementy

---
+++ Dziennik_v0.1/Controllers/Api/AchievementsApiController.cs
@@ -0,0 +1,36 @@
+﻿using Dziennik_v0._1.Core;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Dziennik_v0._1.Controllers.Api
+{
+    public class AchievementsApiController : ApiController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AchievementsApiController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        public IHttpActionResult AchievementsIsNewCount()
+        {
+            var userId = User.Identity.GetUserId();
+            var Achievements = _unitOfWork.Achievements.GetAllAchievement(userId);
+            var IsNewCount=0;
+            foreach (var item in Achievements)
+            {
+                if (item.IsNew)
+                {
+                    IsNewCount++;
+                }
+            }
+            return Ok(IsNewCount);
+        }
+    }
+}

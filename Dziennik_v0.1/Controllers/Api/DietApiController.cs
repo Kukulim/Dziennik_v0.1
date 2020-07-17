354a66fe020788ea265e62ff889395451063e267
kukulim <beckon@interia.pl>
2020-07-17 17:37:33 +0200

    dziala posilki a csv z wszystkim cal i wart odzywcze

--- Dziennik_v0.1/Controllers/Api/DietApiController.cs
+++ Dziennik_v0.1/Controllers/Api/DietApiController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dziennik_v0._1.Core.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -12,8 +14,25 @@ namespace Dziennik_v0._1.Controllers.Api
         [HttpGet]
         public IHttpActionResult GetFoodData()
         {
+            List<FoodModel> foods = new List<FoodModel>();
             string csvData = System.IO.File.ReadAllText("D:/Dziennik_v0.1/Dziennik_v0.1/App_Data/TabeleKaloryczne.csv");
-            return Ok(csvData);
+
+            foreach (string row in csvData.Split('\n'))
+            {
+                if (!string.IsNullOrEmpty(row))
+                {
+                    foods.Add(new FoodModel
+                    {
+                        Name = row.Split('.')[0],
+                        Calories = Convert.ToInt32(row.Split('.')[1]),
+                        Protein = Convert.ToDecimal(row.Split('.')[2]),
+                        Fat = Convert.ToDecimal(row.Split('.')[3]),
+                        Carbohydrates = Convert.ToDecimal(row.Split('.')[4]),
+                    });
+                }
+            }
+                var jsonResault = JsonConvert.SerializeObject(foods);
+            return Json(jsonResault);
         }
     }
 }
---
+++ Dziennik_v0.1/Core/Models/FoodModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dziennik_v0._1.Core.Models
+{
+    public class FoodModel
+    {
+        public string Name { get; set; }
+        public int Calories { get; set; }
+        public decimal Protein { get; set; }
+        public decimal Carbohydrates { get; set; }
+        public decimal Fat { get; set; }
+    }
+}

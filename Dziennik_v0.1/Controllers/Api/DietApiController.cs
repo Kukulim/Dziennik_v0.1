4d48238126d7e5400a1ff79041e46706eb3b6b99
kukulim <beckon@interia.pl>
2020-07-17 15:44:00 +0200

    apidiet do czytania csv

---
+++ Dziennik_v0.1/Controllers/Api/DietApiController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Dziennik_v0._1.Controllers.Api
+{
+    public class DietApiController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetFoodData()
+        {
+            string csvData = System.IO.File.ReadAllText("D:/Dziennik_v0.1/Dziennik_v0.1/App_Data/TabeleKaloryczne.csv");
+            return Ok(csvData);
+        }
+    }
+}
